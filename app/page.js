ea4ec3b7266ff235f845c8cf22a999f0a450162f
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-07-10 10:11:54 +0700

    Final

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
   const testimonials = [
@@ -34,46 +36,51 @@ export default function Home() {
       link: "https://www.behance.net/gallery/209453751/Pertamina-Jogja-Surabaya-Social-Media-Design"
     },
   ];
-
+  const [isHover, setIsHover] = useState(false);
   return (
     <div className="relative min-h-screen bg-black text-white overflow-hidden">
+      {/* Background Ego */}
       <div className="">
         <Image
           src="/back_ego.svg"
-          alt="Vercel Logo"
+          alt="Ego Background"
           width={100}
           height={24}
-          className="absolute w-1/2 h-auto pointer-events-none sm:w-auto"
+          className="absolute w-full sm:w-1/2 h-auto pointer-events-none"
         />
       </div>
 
-      <div className="relative flex justify-end px-36 py-24">
-        <Link href="/works" className="px-6 py-2 hover:underline">Works</Link>
-        <Link href="/about" className="px-6 py-2 hover:underline">About us</Link>
-        <Link href="/contact" className="px-6">
-          <button className="border border-white hover:border-[#F26837] rounded-full px-6 py-2 text-sm transition-all hover:bg-[#F26837] hover:text-white duration-300 flex items-center">
+      {/* Navigation bar */}
+      <div className="relative flex justify-end px-6 sm:px-12 md:px-24 lg:px-36 py-6 sm:py-8 md:py-16 lg:py-24">
+        <Link href="/works" className="px-2 sm:px-4 py-2 hover:underline">Works</Link>
+        <Link href="/about" className="px-2 sm:px-4 py-2 hover:underline">About us</Link>
+        <Link href="/contact" className="px-2 sm:px-4">
+          <button className="border border-white hover:border-[#F26837] rounded-full px-4 sm:px-6 py-2 text-sm transition-all hover:bg-[#F26837] hover:text-white duration-300 flex items-center">
             Contact
           </button>
         </Link>
       </div>
 
-      <main className="flex justify-end items-end h-[40vh] pr-46">
-        <div className="text-right text-[32px] leading-[0.89] tracking-[-0.05em]">
+      {/* Hero Section */}
+      <main className="flex justify-end items-end h-[40vh]">
+        <div className="text-right text-xl sm:text-2xl md:text-3xl leading-[0.89] tracking-[-0.05em] pr-4 sm:pr-8 md:pr-24 lg:pr-46">
           <p>not about my <span className="font-bold italic">ego.</span></p>
           <p>nor about your <span className="font-bold italic">ego.</span></p>
           <p>design is about our brand & customer <span className="font-bold italic">ego.</span></p>
         </div>
       </main>
 
-      <div className="abosolute flex flex-col justify-center items-center h-[80vh]">
+
+      {/* CTA */}
+      <div className="flex flex-col justify-center items-center min-h-[60vh] py-12 sm:py-16 md:py-20 px-4">
         <Image
           src="/logo_desainego.svg"
           alt="Ego Logo"
           width={500}
           height={500}
-          className="flex w-[25%] pointer-events-none h-auto"
+          className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 pointer-events-none h-auto"
         />
-        <div className="text-[32px] text-center leading-[1.04] tracking-[-0.05em] mt-8 font-normal not-italic">
+        <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-center leading-tight tracking-[-0.05em] mt-8 font-normal not-italic">
           <p>our keyword: user-centered.</p>
           <p>we are your B2B global design studio.</p>
           <p>we help your brand match its customer ego.</p>
@@ -81,24 +88,32 @@ export default function Home() {
         </div>
       </div>
 
+
+
+      {/* Clients */}
       <div className="bg-black text-white px-4 pb-20">
         <div className="flex flex-col justify-center gap-y-8">
-          <h2 className="text-[32px] text-start leading-[1.04] tracking-[-0.05em] mb-12 w-full max-w-7xl mx-auto px-24">we’ve collaborated with:</h2>
-          <div className="flex justify-center gap-22 flex-wrap py-6">
+          <h2 className="text-xl sm:text-2xl md:text-3xl text-start leading-[1.2] tracking-[-0.05em] mb-12 w-full max-w-7xl mx-auto px-4 sm:px-8 md:px-24">
+            we’ve collaborated with:
+          </h2>
+
+          <div className="flex justify-center gap-8 sm:gap-12 md:gap-22 flex-wrap py-6">
             <Image src="/brand/BPC.svg" alt="BPC" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/BBF.svg" alt="BBF" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/FSD.svg" alt="FSD" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/JD.svg" alt="JD" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/AB.svg" alt="AB" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
           </div>
-          <div className="flex justify-center gap-20 flex-wrap">
+
+          <div className="flex justify-center gap-8 sm:gap-12 md:gap-20 flex-wrap">
             <Image src="/brand/LB.svg" alt="LB" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/L.svg" alt="L" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/B.svg" alt="B" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/CA.svg" alt="CA" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/UII.svg" alt="UII" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
           </div>
-          <div className="flex justify-center gap-12 flex-wrap py-6">
+
+          <div className="flex justify-center gap-8 sm:gap-12 md:gap-12 flex-wrap py-6">
             <Image src="/brand/AP.svg" alt="AP" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/OA.svg" alt="OA" width={100} height={50} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/WPP.svg" alt="WPP" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
@@ -106,7 +121,8 @@ export default function Home() {
             <Image src="/brand/CC.svg" alt="CocaCola" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/J.svg" alt="Japfa" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
           </div>
-          <div className="flex justify-center gap-16 flex-wrap mr-72">
+
+          <div className="flex justify-center gap-8 sm:gap-12 md:gap-16 flex-wrap mr-0 sm:mr-0 md:mr-72">
             <Image src="/brand/BS.svg" alt="BS" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/OA.svg" alt="OA" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
             <Image src="/brand/P.svg" alt="Pertamina" width={150} height={100} className="object-contain pointer-events-none max-h-20" />
@@ -115,13 +131,12 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Services */}
       <div className="bg-black text-white px-4 py-20">
-        <h2 className="text-[32px] text-start leading-[1.04] tracking-[-0.05em] mb-12 w-full max-w-7xl mx-auto px-24">
-          let’s satisfy our customer (design) ego with <br /><span className="italic">desainego.</span>
+        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-start leading-[1.2] tracking-[-0.05em] mb-12 w-full max-w-7xl mx-auto px-4 sm:px-8 md:px-12 lg:px-24">
+          let’s satisfy our customer (design) ego with <br /><span className="font-bold italic">desainego.</span>
         </h2>
-
-
-        <div className="grid grid-cols-3 gap-y-6 justify-items-center max-w-4xl mx-auto py-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-12 gap-x-4 justify-items-center max-w-4xl mx-auto py-16">
           <div className="flex flex-col items-center text-center">
             <Image src="/service/BLVI.svg" alt="Brand Logo" width={150} height={150} className="pointer-events-none" />
           </div>
@@ -137,12 +152,13 @@ export default function Home() {
           <div className="flex flex-col items-center text-center">
             <Image src="/service/PP.svg" alt="Product" width={150} height={150} className="pointer-events-none" />
           </div>
-          <div className="flex flex-col items-center text-center pt-16">
+          <div className="flex flex-col items-center text-center pt-8 md:pt-16">
             <Image src="/service/AM.svg" alt="And More" width={150} height={150} className="pointer-events-none" />
           </div>
         </div>
       </div>
 
+      {/* Works */}
       <div id="works" className="bg-black text-white pb-16 sm:pb-24 lg:pb-32 overflow-hidden">
         <div className="max-w-6xl mx-auto text-center mb-12 px-10">
           <p className="text-[32px] text-start leading-[1.04] tracking-[-0.05em] pb-20 w-full">
@@ -154,22 +170,22 @@ export default function Home() {
             {[...testimonials, ...testimonials].map((testimonial, index) => (
               <div
                 key={index}
-                className="min-h-[580px] sm:min-h-[580px] w-[280px] sm:w-[350px] md:w-[450px] 
+                className="min-h-[450px] sm:min-h-[580px] w-[280px] sm:w-[350px] md:w-[450px] 
                         p-10 rounded-xl border border-[#F26837] relative flex flex-col justify-start"
               >
                 <div className="flex flex-col gap-4 justify-center items-center">
                   <Image
                     src={testimonial.image}
-                    width={350}
+                    width={365}
                     height={200}
                     alt="testimonial image"
                     className="rounded-lg pointer-events-none object-cover"
                   />
-                  <p className="text-white text-sm sm:text-base leading-relaxed mx-4">{testimonial.text}</p>
+                  <p className="text-white text-sm sm:text-base leading-relaxed">{testimonial.text}</p>
                 </div>
-                <div className="mt-4 mx-4">
+                <div className="mt-4">
                   <Link href={testimonial.link} className="text-white text-sm hover:underline">
-                    Read more →
+                    Read more &gt;
                   </Link>
                 </div>
               </div>
@@ -177,44 +193,68 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      <div className="relative flex flex-col items-end">
-        <div className="">
-          <Link href="/about" className="pr-58 underline">click here for more details...</Link>
-        </div>
+      {/* Click */}
+      <div className="w-full flex justify-end pb-18">
+        <Link
+          href="/about"
+          className="text-xl sm:text-2xl md:text-3xl underline mr-6 sm:mr-12 md:mr-82"
+        >
+          click here for more details...
+        </Link>
       </div>
 
+      {/* Footer Section*/}
       <div className="bg-black text-white px-4 sm:px-6 md:px-20">
-        <div className="max-w-7xl mx-auto pt-20 pb-6">
+        <div className="max-w-7xl mx-auto md:pt-20 pb-6">
+          {/* Big Button */}
           <div className="flex flex-col items-center justify-center mb-12">
-            <button className="bg-[#F26837] sm:min-h-[144px] md:w-[1050px] flex items-center justify-center text-white text-4xl font-bold mx-auto px-10 py-4 transition">
+            <button
+              className="bg-[#F26837] hover:bg-white sm:min-h-[144px] md:w-[1050px] flex items-center justify-center text-white text-4xl font-bold mx-auto px-10 py-4 transition"
+              onMouseEnter={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+            >
               <Image
-                src="/back_letscollab.svg"
+                src={isHover ? "/back_letscollab_org.svg" : "/back_letscollab.svg"}
                 alt=""
                 width={250}
                 height={150}
-                className="mx-auto"
+                className="mx-auto transition-all duration-300"
               />
             </button>
+
           </div>
+          {/* Social Media and Logo */}
           <div className="flex flex-col md:flex-row justify-between items-center mx-30 gap-10 mb-12">
             <div className="flex flex-col items-center md:items-start gap-6">
               <p className="text-sm">let’s connect on social media:</p>
-              <div className="flex gap-6">
-                {["be", "ig", "x", "linkedin", "tt"].map((icon) => (
-                  <Link href="#" key={icon}>
+              <div className="flex gap-3 sm:gap-4 md:gap-6">
+                {[
+                  { icon: "be", href: "https://behance.net/emfahrurozi" },
+                  { icon: "ig", href: "https://instagram.com/desainego_" },
+                  { icon: "x", href: "https://x.com/emfahrurozi" },
+                  { icon: "linkedin", href: "https://www.linkedin.com/company/desainego" },
+                  { icon: "tt", href: "https://www.tiktok.com/@desainego" },
+                ].map((item) => (
+                  <Link
+                    href={item.href}
+                    key={item.icon}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <Image
-                      src={`/icon/ic_${icon}.svg`}
-                      alt={icon}
-                      width={24}
-                      height={24}
-                      className="hover:scale-110 transition-transform duration-200"
+                      src={`/icon/ic_${item.icon}.svg`}
+                      alt={item.icon}
+                      width={32}
+                      height={32}
+                      className="w-10 sm:w-8 md:w-6 hover:scale-110 transition-transform duration-200"
                     />
                   </Link>
                 ))}
               </div>
-            </div>
 
+
+            </div>
+            {/* Logo & Text */}
             <div className="flex flex-col items-center md:items-end">
               <Image
                 src="/logo_desainego.svg"
